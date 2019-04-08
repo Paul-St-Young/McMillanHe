5f50d4694e7b4e2f726f81a37686c8768306178f
Paul Young <yyang173@illinois.edu>
2019-04-07 21:08:35 -0500

    check wf ratio

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "mmh.h"
 
 using namespace std;
@@ -31,14 +32,32 @@ void test_gl()
     pos(iatom, idim) -= dr;
     lap1 = (wf1+wf2-2*wf0)/pow(dr, 2);
     lap += lap1;
-    //cout << wf1 << " " << wf0 << " " << wf2 << endl;
-    //cout << lap1 << endl;
   }
-  cout << "laplacian: " << lap << endl;
+  cout << lap << endl;
+}
+
+void test_ratio()
+{
+  int natom=2, ndim=3;
+  double wf0, wf1, wf2, dr=0.2;
+  McMillanHe mmh = McMillanHe();
+  McMillanHe::Matrix pos(natom, ndim);
+  McMillanHe::Vector move(ndim);
+  move(0) = dr;
+  pos(0, 0) = -1.0;
+  pos(1, 0) = 1.0;
+  wf0 = mmh.lnwf(pos);
+  int iatom = 0; // atom to move
+  cout << mmh.ratio(pos, move, iatom) << endl;
+  // calculate FD ratio
+  pos.row(iatom) += move;
+  wf1 = mmh.lnwf(pos);
+  cout << exp(wf1-wf0) << endl;
 }
 
 int main(int argc, char** argv)
 {
-  test_gl();
+  //test_gl();
+  test_ratio();
   return 0;
 }
